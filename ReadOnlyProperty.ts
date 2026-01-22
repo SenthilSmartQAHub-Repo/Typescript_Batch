51371a031fc1cd3491025259d5e29ba01fd60a0f
senthil <thangarasu.senthil05@gmail.com>
2026-01-22 21:22:56 +0530

    readonly

---
+++ ReadOnlyProperty.ts
@@ -0,0 +1,11 @@
+
+// readonly is a keyword used to declared a variable(property) inside the class in typescript
+// When a variable is declared and initialized using readonly then the value can't be modified
+// readonly like a const
+
+class Person
+{
+    readonly accountNumber:number=12345;
+ 
+
+}
